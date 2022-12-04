309c8b478a07027a76cdd059a2d6c033cbb9af40
Matthew Baldwin <baldwinm@amazon.com>
2022-12-04 14:26:02 +0000

    Minor template mod

--- template/main.cpp
+++ template/main.cpp
@@ -10,10 +10,15 @@ int main(int argc, char** argv) {
   std::unique_ptr<MappedFileSource>m(new MappedFileSource(argc, argv));
   std::string_view f(m->data(), m->size());
 
+  size_t part1 = 0;
+  size_t part2 = 0;
+
   std::string_view line;
   while (aoc::getline(f, line, "\r\n")) {
     DEBUG_LOG(line);
   }
+
+  aoc::print_results(part1, part2);
 
   return 0;
 }
