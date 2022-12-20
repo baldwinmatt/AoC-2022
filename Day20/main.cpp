2a4d8d4fe5c2c2b32feb4a5b8c21338b08fa8dec
Matthew Baldwin <baldwinm@amazon.com>
2022-12-20 15:12:48 +0000

    Day20 - part 2

--- Day20/main.cpp
+++ Day20/main.cpp
@@ -13,10 +13,10 @@ namespace {
 0
 4)");
   constexpr int SR_Part1 = 3;
-  constexpr int SR_Part2 = 0;
+  constexpr int SR_Part2 = 1623178306;
 
   struct Node {
-    int val;
+    int64_t val;
     Node *next;
     Node *prev;
 
@@ -65,12 +65,56 @@ namespace {
       }
     }
 
+    MixList(const MixList& o)
+      : list(nullptr)
+      , zero(nullptr)
+      , size(o.size)
+    {
+      list = new Node[size];
+
+      // update pointers
+      for (size_t i = 0; i < size; i++) {
+        list[i].val = o.list[i].val;
+        if (!list[i].val) {
+          zero = &list[i];
+        }
+        const size_t prev = (i > 0) ? (i - 1) : (size - 1);
+        const size_t next = (i < size - 1) ? i + 1 : 0;
+
+        list[i].prev = &list[prev];
+        list[i].next = &list[next];
+      }
+    }
+
     ~MixList() {
       delete [] list;
 
       list = nullptr;
       zero = nullptr;
       size = 0;
+    }
+
+    MixList& operator=(const MixList& o) {
+      delete [] list;
+
+      zero = nullptr;
+      size = o.size;
+      list = new Node[size];
+
+      // update pointers
+      for (size_t i = 0; i < size; i++) {
+        list[i].val = o.list[i].val;
+        if (!list[i].val) {
+          zero = &list[i];
+        }
+        const size_t prev = (i > 0) ? (i - 1) : (size - 1);
+        const size_t next = (i < size - 1) ? i + 1 : 0;
+
+        list[i].prev = &list[prev];
+        list[i].next = &list[next];
+      }
+
+      return *this;
     }
   };
 
@@ -79,7 +123,51 @@ namespace {
     aoc::parse_as_integers(f, "\r\n", [&in](const int64_t n) {
       in.push_back(n);
     });
-    return std::make_shared<MixList>(in);
+    return MixList{in};
+  };
+
+  const auto mix = [](MixList& r, size_t rounds, int64_t key) {
+    aoc::AutoTimer mixing{"Mixing"};
+
+    key %= (r.size - 1);
+
+    for (size_t n = 0; n < rounds; n++) {
+      for (size_t i = 0; i < r.size; i++) {
+        Node *cur = &r.list[i];
+
+        if (!cur->val) {
+          continue;
+        }
+
+        Node *p = cur->prev;
+
+        // remove
+        cur->prev->next = cur->next;
+        cur->next->prev = cur->prev;
+
+        const int64_t val = cur->val * key;
+        DEBUG_LOG(i, val);
+        int64_t dist = std::abs(val) % (r.size - 1);
+
+        // walk to new location
+        if (val > 0) {
+          while (dist--) {
+            p = p->next;
+          }
+        } else {
+          while (dist--) {
+            p = p->prev;
+          }
+        }
+        // insert
+        cur->prev = p;
+        cur->next = p->next;
+
+        // update pointers
+        cur->prev->next = cur;
+        cur->next->prev = cur;
+      }
+    }
   };
 }
 
@@ -87,7 +175,7 @@ int main(int argc, char** argv) {
   aoc::AutoTimer t;
   const bool inTest = argc < 2;
 
-  std::shared_ptr<MixList>r;
+  MixList r;
   if (inTest) {
     r = LoadInput(SampleInput);
   } else {
@@ -96,54 +184,18 @@ int main(int argc, char** argv) {
     r = LoadInput(f);
   }
 
-  assert(r->zero);
-  assert(r->list);
-  assert(r->size);
+  assert(r.zero);
+  assert(r.list);
+  assert(r.size);
 
-  int part1 = 0;
-  int part2 = 0;
-
-  {
-    aoc::AutoTimer mixing{"Mixing"};
-
-    for (size_t i = 0; i < r->size; i++) {
-      Node *cur = &r->list[i];
-      DEBUG_LOG(i, cur->val);
-
-      if (!cur->val) {
-        continue;
-      }
-
-      Node *p = cur->prev;
-
-      // remove
-      cur->prev->next = cur->next;
-      cur->next->prev = cur->prev;
-
-      int dist = std::abs(cur->val) % (r->size - 1);
-
-      // walk to new location
-      if (cur->val > 0) {
-        while (dist--) {
-          p = p->next;
-        }
-      } else {
-        while (dist--) {
-          p = p->prev;
-        }
-      }
-      // insert
-      cur->prev = p;
-      cur->next = p->next;
-
-      // update pointers
-      cur->prev->next = cur;
-      cur->next->prev = cur;
-    }
-  }
+  int64_t part1 = 0;
+  int64_t part2 = 0;
 
   {
-    Node *p = r->zero;
+    MixList r1 = r;
+    mix(r1, 1, 1);
+
+    Node *p = r1.zero;
     int n = 0;
     size_t i = 0;
     while (n < 3) {
@@ -155,6 +207,26 @@ int main(int argc, char** argv) {
       p = p->next;
       i++;
     }
+  }
+
+  {
+    MixList r2 = r;
+    
+    mix(r2, 10, 811589153);
+
+    Node *p = r2.zero;
+    int n = 0;
+    size_t i = 0;
+    while (n < 3) {
+      if (i && (i % 1000 == 0)) {
+        n++;
+        DEBUG_LOG(i, p->val);
+        part2 += p->val;
+      }
+      p = p->next;
+      i++;
+    }
+    part2 *= 811589153;
   }
 
   aoc::print_results(part1, part2);
